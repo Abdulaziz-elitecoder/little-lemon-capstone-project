1c3c363db289f1cfd61566046b970cf74eb82bc1
Abdulaziz-elitecoder <abdulaziz.muhammed.elnoti@gmail.com>
2023-12-22 22:43:32 +0200

    testing authentication

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = routers.DefaultRouter()
@@ -27,4 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/',include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
+
 ]
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from .serializers import *
 from .models import *
 from rest_framework import generics , viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class MenuItemsView(generics.ListAPIView):
@@ -15,4 +18,10 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView)
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is xml"})
